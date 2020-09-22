daf130214d8d8d4a263287bb8258ba639a9e061b
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-21 23:45:46 -0500

    completed question 1.1

--- src/CCI.java
+++ src/CCI.java
@@ -1,11 +1,34 @@
 public class CCI {
 
-    public int positiveIntegerSolutions (int a, int b, int c) {
+    /*public int positiveIntegerSolutions (int a, int b, int c) {
 
         return
+    }*/
+
+    // 1.1 Is Unique: implement an algorithm to determine if a string has all unique characters
+    //what if you can't use additional data structures?
+
+    public static boolean isUniqueChars(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if (char_set[val]) return false;
+            char_set[val] = true;
+        }
+        return true;
     }
 
+
     public static void main(String[] args) {
+        //test for question 1.1
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        for (String word : words) {
+            System.out.println(word + ": " + isUniqueChars(word));
+        }
+
 
     }
 }
