df8b91547652d37096d2a010a17ebbb14c460754
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-23 23:58:30 -0500

    added alternative enhanced solution for permutation

--- src/CCI.java
+++ src/CCI.java
@@ -113,6 +113,25 @@ public class CCI {
         return sort(s).equals(sort(t));
     }
 
+    // 1.2 B permutation alternative
+    public static boolean permutationB(String s, String t) {
+        if (s.length() != t.length()) return false; // Permutations must be same length
+
+        int[] letters = new int[128]; // Assumption: ASCII
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i)]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            letters[t.charAt(i)]--;
+            if (letters[t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+
+        return true; // letters array has no negative values, and therefore no positive values either
+    }
+
 
     public static void main(String[] args) {
         //test for question 1.1
@@ -129,6 +148,8 @@ public class CCI {
             boolean anagram = permutation(word1, word2);
             System.out.println(word1 + ", " + word2 + ": " + anagram);
         }
+
+
 
 
     }
