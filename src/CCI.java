2ef2f03f076f4c176038c49c3186038159680626
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-24 23:47:45 -0500

    completed question 1.3

--- src/CCI.java
+++ src/CCI.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 
+
 public class CCI {
 
 //-------------------------------    Key Topics to Study & Master -----------------------------------------
@@ -132,6 +133,39 @@ public class CCI {
         return true; // letters array has no negative values, and therefore no positive values either
     }
 
+    //question 1.3
+    // Assume string has sufficient free space at the end
+    public static void replaceSpaces(char[] str, int trueLength) {
+        int spaceCount = 0, index, i = 0;
+        for (i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length) str[trueLength] = '\0';
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+    }
+    //question 1.3 part B
+    public static int findLastCharacter(char[] str) {
+        for (int i = str.length - 1; i >= 0; i--) {
+            if (str[i] != ' ') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         //test for question 1.1
@@ -148,6 +182,8 @@ public class CCI {
             boolean anagram = permutation(word1, word2);
             System.out.println(word1 + ", " + word2 + ": " + anagram);
         }
+
+
 
 
 
